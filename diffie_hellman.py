f8d03569dd8956c86652999e2756b6bb7622a073
Sina Jabbarpour <jabbarpoursina@gmail.com>
2023-02-13 18:20:00 +0330

    result

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -1,13 +1,13 @@
 import secrets
 
 
-def private_key(p):
-    return secrets.randbelow(p-2) + 1
+def private_key(p: int):
+    return secrets.randbelow(p-2) + 2
 
 
-def public_key(p, g, private):
+def public_key(p: int, g: int, private: int) -> int:
     return (g**private) % p
 
 
-def secret(p, public, private):
+def secret(p: int, public: int, private: int) -> int:
     return (public**private) % p
